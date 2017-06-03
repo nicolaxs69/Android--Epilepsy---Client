060eaff8c0cef0c31372e0acce44bcc9377de6de
nicolaxs69 <nicoescobar69@gmail.com>
2017-06-03 13:18:53 -0500

    Login and Bottom bar

---
+++ app/src/main/java/com/example/bottombar/sample/TabMessage.java
@@ -0,0 +1,29 @@
+package com.example.bottombar.sample;
+
+/**
+ * Created by iiro on 7.6.2016.
+ */
+public class TabMessage {
+    public static String get(int menuItemId, boolean isReselection) {
+        String message = "Content for ";
+
+        switch (menuItemId) {
+
+            case R.id.tab_favorites:
+                message += "favorites";
+                break;
+            case R.id.tab_nearby:
+                message += "nearby";
+                break;
+            case R.id.tab_friends:
+                message += "friends";
+                break;
+        }
+
+        if (isReselection) {
+            message += " WAS RESELECTED! YAY!";
+        }
+
+        return message;
+    }
+}
---
+++ app/src/main/java/com/example/bottombar/sample/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.bottombar.sample.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://warm-beyond-22026.herokuapp.com/api/v1/";
+    public static final String TOKEN = "token";
+    public static final String EMAIL = "email";
+}
